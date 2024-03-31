5cec944db5251daede2554da12367bfadf7c13d8
Koy <93858393+charleess15@users.noreply.github.com>
2024-03-31 19:09:31 +0800

    Added ReactJS + Tailwind CSS + Components
    Added Header & Footer (No Test Suites Yet)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from './Footer.jsx'
+import Header from './Header.jsx'
+
+function App(){
+
+  return(
+    <>
+      <Header/>
+      <div className="bg-[#b3735b] w-full h-screen">
+
+      </div>
+      <Footer/>
+    </>
+  );
+}
+
+export default App
