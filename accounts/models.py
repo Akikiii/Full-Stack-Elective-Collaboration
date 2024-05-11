c4534c095132c80e353be9e32dc2de5154c733a8
Akikiii <woofmeowie@gmail.com>
2024-05-11 16:55:16 +0400

    Repushed Template
    - Readded Templated

--- accounts/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-# Create your models here.
-class Employee(AbstractUser):
-        has_access=models.BooleanField(default=True)
